ca4e24add55cdd4ac262e7f4add3e4d47bd67868
MORI Kazutaka <mori.kazutaka@gmail.com>
2026-05-06 01:39:51 +0900

    ユーザー名の変更

--- my_scripts/install_coding_fonts.sh
+++ my_scripts/install_coding_fonts.sh
@@ -5,27 +5,27 @@ set -x
 
 ## UDEV Gothic LG (BIZ UD Gothic + JetBrains Mono)
 ##   https://github.com/yuru7/udev-gothic
-mkdir -p /home/rstudio/.config/rstudio/fonts/UDEVGothicLG/400/italic
-mkdir -p /home/rstudio/.config/rstudio/fonts/UDEVGothicLG/700/italic
+mkdir -p /home/${DEFAULT_USER}/.config/rstudio/fonts/UDEVGothicLG/400/italic
+mkdir -p /home/${DEFAULT_USER}/.config/rstudio/fonts/UDEVGothicLG/700/italic
 wget -q https://github.com/yuru7/udev-gothic/releases/download/v2.2.0/UDEVGothic_v2.2.0.zip -O UDEVGothic.zip
 unzip -j -d UDEVGothic UDEVGothic.zip
-cp UDEVGothic/UDEVGothicLG-Regular.ttf /home/rstudio/.config/rstudio/fonts/UDEVGothicLG/400
-cp UDEVGothic/UDEVGothicLG-Italic.ttf /home/rstudio/.config/rstudio/fonts/UDEVGothicLG/400/italic
-cp UDEVGothic/UDEVGothicLG-Bold.ttf /home/rstudio/.config/rstudio/fonts/UDEVGothicLG/700
-cp UDEVGothic/UDEVGothicLG-BoldItalic.ttf /home/rstudio/.config/rstudio/fonts/UDEVGothicLG/700/italic
-mv /home/rstudio/.config/rstudio/fonts/UDEVGothicLG/ /home/rstudio/.config/rstudio/fonts/UDEV\ Gothic\ LG/
+cp UDEVGothic/UDEVGothicLG-Regular.ttf /home/${DEFAULT_USER}/.config/rstudio/fonts/UDEVGothicLG/400
+cp UDEVGothic/UDEVGothicLG-Italic.ttf /home/${DEFAULT_USER}/.config/rstudio/fonts/UDEVGothicLG/400/italic
+cp UDEVGothic/UDEVGothicLG-Bold.ttf /home/${DEFAULT_USER}/.config/rstudio/fonts/UDEVGothicLG/700
+cp UDEVGothic/UDEVGothicLG-BoldItalic.ttf /home/${DEFAULT_USER}/.config/rstudio/fonts/UDEVGothicLG/700/italic
+mv /home/${DEFAULT_USER}/.config/rstudio/fonts/UDEVGothicLG/ /home/${DEFAULT_USER}/.config/rstudio/fonts/UDEV\ Gothic\ LG/
 rm -rf UDEVGothic*
 
 ## Mint Mono (Intel One Gothic + Circle M+, etc.)
 ##   https://github.com/yuru7/mint-mono
-mkdir -p /home/rstudio/.config/rstudio/fonts/MintMono/400/italic
-mkdir -p /home/rstudio/.config/rstudio/fonts/MintMono/700/italic
+mkdir -p /home/${DEFAULT_USER}/.config/rstudio/fonts/MintMono/400/italic
+mkdir -p /home/${DEFAULT_USER}/.config/rstudio/fonts/MintMono/700/italic
 wget -q https://github.com/yuru7/mint-mono/releases/download/v0.0.1/MintMono_v0.0.1.zip -O MintMono.zip
 unzip -j -d MintMono MintMono.zip
-cp MintMono/MintMono-Regular.ttf /home/rstudio/.config/rstudio/fonts/MintMono/400
-cp MintMono/MintMono-Italic.ttf /home/rstudio/.config/rstudio/fonts/MintMono/400/italic
-cp MintMono/MintMono-Bold.ttf /home/rstudio/.config/rstudio/fonts/MintMono/700
-cp MintMono/MintMono-BoldItalic.ttf /home/rstudio/.config/rstudio/fonts/MintMono/700/italic
+cp MintMono/MintMono-Regular.ttf /home/${DEFAULT_USER}/.config/rstudio/fonts/MintMono/400
+cp MintMono/MintMono-Italic.ttf /home/${DEFAULT_USER}/.config/rstudio/fonts/MintMono/400/italic
+cp MintMono/MintMono-Bold.ttf /home/${DEFAULT_USER}/.config/rstudio/fonts/MintMono/700
+cp MintMono/MintMono-BoldItalic.ttf /home/${DEFAULT_USER}/.config/rstudio/fonts/MintMono/700/italic
 rm -rf MintMono*
 
-chown -R rstudio:rstudio /home/rstudio/.config/rstudio
+chown -R ${DEFAULT_USER}:${DEFAULT_USER} /home/${DEFAULT_USER}/.config/rstudio
